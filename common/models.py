fd448348f8f18feda3a185c66113d2b477555220
Invisible Golf <dev@invisibleinstitute.com>
2015-05-13 12:13:29 +0700

    migrated to geomodels

--- common/models.py
+++ common/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import IntegerField
-from django.db.models.base import Model
-from django.db.models.fields import CharField, DateTimeField, DateField, FloatField
+from django.contrib.gis.db import models
 from django.db.models.fields.related import ForeignKey
 
 
@@ -9,70 +7,84 @@ class User(AbstractUser):
     pass
 
 
-class Officer(Model):
-    officer_first = CharField(max_length=255, null=True)
-    officer_last = CharField(max_length=255, null=True)
-    gender = CharField(max_length=1, null=True)
-    race = CharField(max_length=50, null=True)
-    appt_date = DateField(null=True)
-    unit = CharField(max_length=5, null=True)
-    rank = CharField(max_length=5, null=True)
-    star = FloatField(null=True)
+class Officer(models.Model):
+    officer_first = models.CharField(max_length=255, null=True)
+    officer_last = models.CharField(max_length=255, null=True)
+    gender = models.CharField(max_length=1, null=True)
+    race = models.CharField(max_length=50, null=True)
+    appt_date = models.DateField(null=True)
+    unit = models.CharField(max_length=5, null=True)
+    rank = models.CharField(max_length=5, null=True)
+    star = models.FloatField(null=True)
 
     def __str__(self):
         return "%(last)s %(first)s" % {'last':self.officer_last,'first':self.officer_first} 
 
-class OfficerHistory(Model):
+class OfficerHistory(models.Model):
     officer = ForeignKey(Officer, null=True)
-    unit = CharField(max_length=5, null=True)
-    rank = CharField(max_length=5, null=True)
-    star = FloatField(null=True)
-    as_of = DateField(null=True)
+    unit = models.CharField(max_length=5, null=True)
+    rank = models.CharField(max_length=5, null=True)
+    star = models.FloatField(null=True)
+    as_of = models.DateField(null=True)
 
 
-class PoliceWitness(Model):
-    pwit_id = IntegerField(primary_key=True)
-    crid = CharField(max_length=30, null=True)
-    gender = CharField(max_length=1, null=True)
-    race = CharField(max_length=50, null=True)
+class PoliceWitness(models.Model):
+    pwit_id = models.IntegerField(primary_key=True)
+    crid = models.CharField(max_length=30, null=True)
+    gender = models.CharField(max_length=1, null=True)
+    race = models.CharField(max_length=50, null=True)
     officer = ForeignKey(Officer, null=True)
     
 
 
-class ComplainingWitness(Model):
-    cwit_id = IntegerField(primary_key=True)
-    crid = CharField(max_length=30, null=True)
-    gender = CharField(max_length=1, null=True)
-    race = CharField(max_length=50, null=True)
+class ComplainingWitness(models.Model):
+    cwit_id = models.IntegerField(primary_key=True)
+    crid = models.CharField(max_length=30, null=True)
+    gender = models.CharField(max_length=1, null=True)
+    race = models.CharField(max_length=50, null=True)
 
 
-class AllegationCategory(Model):
-    cat_id = CharField(primary_key=True, max_length=255)
-    category = CharField(max_length=255, null=True)
-    allegation_name = CharField(max_length=255, null=True)
+class AllegationCategory(models.Model):
+    cat_id = models.CharField(primary_key=True, max_length=255)
+    category = models.CharField(max_length=255, null=True)
+    allegation_name = models.CharField(max_length=255, null=True)
     
     def __str__(self):
         return self.allegation_name
 
 
-class Allegation(Model):
-    record_id = IntegerField(null=True)
-    crid = CharField(max_length=30, null=True)
+class Beat(models.Model):
+    name = models.CharField(max_length=100)
+    polygon = models.MultiPolygonField(srid=4326)
+    objects = models.GeoManager()
+
+
+class Allegation(models.Model):
+    record_id = models.IntegerField(null=True)
+    crid = models.CharField(max_length=30, null=True)
     officer = ForeignKey(Officer, null=True)
     cat = ForeignKey(AllegationCategory, null=True)
-    recc_finding = CharField(max_length=255, null=True)
-    recc_outcome = CharField(max_length=5, null=True)
-    final_finding = CharField(max_length=255, null=True)
-    final_outcome = CharField(max_length=5, null=True)
-    beat = CharField(max_length=4, null=True)
-    location = CharField(max_length=20, null=True)
-    add1 = IntegerField(null=True)
-    add2 = CharField(max_length=255, null=True)
-    city = CharField(max_length=255, null=True)
-    incident_date = DateTimeField(null=True)
-    start_date = DateField(null=True)
-    end_date = DateField(null=True)
-    investigator = CharField(max_length=255, null=True)
+    recc_finding = models.CharField(max_length=255, null=True)
+    recc_outcome = models.CharField(max_length=5, null=True)
+    final_finding = models.CharField(max_length=255, null=True)
+    final_outcome = models.CharField(max_length=5, null=True)
+    beat = ForeignKey('Beat',null=True)
+    location = models.CharField(max_length=20, null=True)
+    add1 = models.IntegerField(null=True)
+    add2 = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=255, null=True)
+    incident_date = models.DateTimeField(null=True)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    investigator = models.CharField(max_length=255, null=True)
+    point = models.PointField(srid=4326,null=True,blank=True)
+    objects = models.GeoManager()
+    
+    def save(self,*args,**kwargs):
+        if self.location and not self.point:
+            #geolocate
+            pass
+        super(Allegation,self).save(*args,**kwargs)
     
     def __str__(self):
         return "%s" % self.crid 
