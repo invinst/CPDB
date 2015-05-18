21578e59b1e56a23ef3fa501bc557ed08a391b97
Invisible Cop+Invisible Uniform <dev@invisibleinstitute.com>
2015-05-18 16:38:44 +0700

    Remove old autocomplete view

--- allegation/urls.py
+++ allegation/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls import include, url
-from allegation.views import AllegationAPIView, AllegationAutocompleteJSONView
+from django.conf.urls import url
+from allegation.views import AllegationAPIView
 
 urlpatterns = [
-    url(r'^allegations/autocomplete/', AllegationAutocompleteJSONView.as_view(), name='autocomplete_json'),
     url(r'^api/allegations/', AllegationAPIView.as_view(), name='allegation-api'),
 ]
--- allegation/views.py
+++ allegation/views.py
@@ -1,60 +1,18 @@
-import json
 import re
 
 from django.conf import settings
-from django.core.serializers.json import DjangoJSONEncoder
-from django.core.urlresolvers import reverse
-from django.db.models.functions import Concat
 from django.db.models.query_utils import Q
 from django.http.response import HttpResponse
 from django.views.generic.base import View
 from django.views.generic.list import ListView
 
 from common.json_serializer import JSONSerializer
-from common.models import Allegation, Officer, AllegationCategory
-
-
-autocomplete_fields = [
-    Officer._meta.get_field('officer_first'),
-    Officer._meta.get_field('officer_last'),
-    AllegationCategory._meta.get_field('category'),
-    Allegation._meta.get_field('start_date'),
-    Allegation._meta.get_field('end_date'),
-    Allegation._meta.get_field('crid'),
-    Allegation._meta.get_field('final_outcome'),
-]
-autocomplete_field_verbose = [x.verbose_name for x in autocomplete_fields]
-autocomplete_field_names = [x.name for x in autocomplete_fields]
-autocomplete_field_verbose_to_names = dict(zip(autocomplete_field_verbose, autocomplete_field_names))
-autocomplete_field_name_to_models = {x.name: x.model for x in autocomplete_fields}
+from common.models import Allegation
 
 
 class AllegationListView(ListView):
     model = Allegation
     template_name = 'allegation_list.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(AllegationListView, self).get_context_data(**kwargs)
-        context['autocomplete_json_url'] = reverse('allegation:autocomplete_json')
-        context['searchable_fields'] = autocomplete_field_verbose
-        return context
-
-
-class AllegationAutocompleteJSONView(View):
-    def get(self, request):
-        suggestions = []
-        if 'field' in request.GET and request.GET.get('field') in autocomplete_field_verbose:
-            field = autocomplete_field_verbose_to_names[request.GET.get('field')]
-            model = autocomplete_field_name_to_models[field]
-
-            if 'query' in request.GET:
-                suggestions = model.objects.all().filter(**{
-                    field + '__icontains': request.GET.get('query')
-                }).values_list(field, flat=True)
-            else:
-                suggestions = model.objects.all()[:10].values_list(field)
-
-        return HttpResponse(json.dumps(list(suggestions), cls=DjangoJSONEncoder))
 
 
 class AllegationAPIView(View):
