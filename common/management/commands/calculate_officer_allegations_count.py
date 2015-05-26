75d63a8654aa696d1d46a956df50558df5565269
Invisible Golf <dev@invisibleinstitute.com>
2015-05-26 12:02:55 +0700

    [f] 644a -Improve autocomplete suggestion - remove unused import
    https://www.pivotaltracker.com/story/show/95344538

--- common/management/commands/calculate_officer_allegations_count.py
+++ common/management/commands/calculate_officer_allegations_count.py
@@ -1,10 +1,7 @@
-import os
 from django.core.management.base import BaseCommand
-from django.db import connection
 from django.db.models import Count
 
 from common.models import Officer
-from cpdb.settings.base import BASE_DIR
 
 
 class Command(BaseCommand):
