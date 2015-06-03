ab0815c637f77dc2cba797f5d5d184bb368ee663
Invisible Cop+Invisible Uniform <dev@invisibleinstitute.com>
2015-06-03 16:02:38 +0700

    Update document view & template

--- document/views.py
+++ document/views.py
@@ -1,18 +1,17 @@
 from django.http.response import HttpResponseBadRequest
 from django.shortcuts import render
+from common.models import Allegation
 
 
 def detail_view(request):
-    # if 'id' not in request.GET:
-    #     return HttpResponseBadRequest()
-    # if 'title' not in request.GET:
-    #     return HttpResponseBadRequest()
-    # doc_id = request.GET.get('id')
-    # title = request.GET.get('title')
-    doc_id = '2092482'
-    title = '000_nanana'
+    if 'id' not in request.GET:
+        return HttpResponseBadRequest()
+    allegation_id = request.GET.get('id')
+
+    allegation = Allegation.objects.get(id=allegation_id)
 
     return render(request, 'document/view.html', context={
-        'id': doc_id,
-        'title': title,
+        'id': allegation.document_id,
+        'normalized_title': allegation.document_normalized_title,
+        'title': allegation.document_title,
     })
