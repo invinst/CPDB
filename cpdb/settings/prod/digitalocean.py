4af0aac4dfe921b08e51f2e7bb006230ceb2181c
Invisible Golf <dev@invisibleinstitute.com>
2015-07-02 11:04:32 +0700

    [b] 056 - some documents are linking to a wrong file on document cloud
    - fix wrong env name for db user
    
    https://www.pivotaltracker.com/story/show/97359056

--- cpdb/settings/prod/digitalocean.py
+++ cpdb/settings/prod/digitalocean.py
@@ -18,7 +18,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'cpdb',
-        'USER': os.environ.get('DB_HOST'),
+        'USER': os.environ.get('DB_USER'),
         ***REMOVED***
         'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
