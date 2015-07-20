953d0ff33c337b0d73aa4f8260b2eb490034e46c
Invisible Golf+Invisible Scream Cat <dev@invisibleinstitute.com>
2015-07-20 14:13:38 +0700

    [f] 436 - when we do not have any officer cards to display
    - Refactor the OfficerPage, extract the RelatedOfficer to another components 
    - Whenever we don’t have related officers, don’t show the title
    
    https://www.pivotaltracker.com/story/show/99004436

---
+++ allegation/static/allegation/js/components/Officer/RelatedOfficers.react.js
@@ -0,0 +1,39 @@
+var React = require('react');
+var Officer = require('../Officer.react');
+var _ = require('lodash');
+
+
+var RelatedOfficers = React.createClass({
+
+  getInitialState: function () {
+    return { 'relatedOfficers': {} };
+  },
+
+  renderRelatedOfficerList: function() {
+    return _.map(this.props.relatedOfficers, function(officer) {
+      return (
+        <div className='col-md-2'>
+          <Officer officer={officer} noClick={true} active={true}/>
+        </div>
+      );
+    });
+  },
+
+  renderTitle: function() {
+    var title = this.props.relatedOfficers.length ? 'Related Officers' : '';
+    return (<div className='row'><h3 className='col-md-12'>{title}</h3></div>);
+  },
+
+  render: function () {
+    return (
+      <div>
+        {this.renderTitle() }
+        <div className='row'>
+          {this.renderRelatedOfficerList()}
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = RelatedOfficers;
