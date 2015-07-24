12c5fb910ed95fc26bc0c19c8dc8960438d159d4
Invisible Cop <dev@invisibleinstitute.com>
2015-07-24 08:52:14 +0700

    [f] 402 - Search by complainant field
    - add unit test for suggest view
    
    https://www.pivotaltracker.com/story/show/99683402

--- search/tests/views/test_suggest_view.py
+++ search/tests/views/test_suggest_view.py
@@ -104,3 +104,11 @@ class SuggestViewTestCase(SimpleTestCase):
         data = self.get_suggestion('Unfo')
         data.should.contain('final_finding')
         data.should.contain('recc_finding')
+
+    def test_suggest_complainant_gender(self):
+        data = self.get_suggestion('mal')
+        data.should.contain('complainant_gender')
+
+    def test_suggest_complainant_race(self):
+        data = self.get_suggestion('blac')
+        data.should.contain('complainant_race')
