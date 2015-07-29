170aa77a2964af55e00d666d4754cf1ab6a160a5
Invisible Cop <dev@invisibleinstitute.com>
2015-07-29 16:20:42 +0700

    [f] 560 - upon clicking the request button
    - send notification to email when document is available 
    
    https://www.pivotaltracker.com/story/show/97078560

--- common/management/commands/update_documents.py
+++ common/management/commands/update_documents.py
@@ -1,7 +1,8 @@
-import json
 from django.core.management.base import BaseCommand
+
 from documentcloud import DocumentCloud
 from common.models import Allegation
+from document.utils import send_document_notification
 
 
 class Command(BaseCommand):
@@ -25,22 +26,42 @@ class Command(BaseCommand):
         else:
             allegations = Allegation.objects.filter(id__gte=start)
 
+        document_by_crid = {}
+
         client = DocumentCloud()
         for allegation in allegations:
-            objs = client.documents.search(self.search_syntax % allegation.crid)
-            if len(objs) > 0:
-                obj = objs[0]
+            document = None
 
-                id_parts = obj.id.split(self.id_delim)
+            if allegation.crid in document_by_crid:
+                document = document_by_crid[allegation.crid]
+            else:
+                objs = client.documents.search(self.search_syntax % allegation.crid)
+                if len(objs) > 0:
+                    document = objs[0]
+                    document_by_crid[allegation.crid] = document
+
+                    if document.title != allegation.document_title:  # new document found
+                        # send notification
+                        send_document_notification(allegation, document)
+                else:
+                    document_by_crid[allegation.crid] = None
+
+            if document:
+                document_by_crid[allegation.crid] = document
+
+                id_parts = document.id.split(self.id_delim)
                 doc_id = id_parts[0]
                 normalized_title = self.id_delim.join(id_parts[1:])
-                title = obj.title
+                title = document.title
 
                 allegation.document_id = doc_id
                 allegation.document_normalized_title = normalized_title
                 allegation.document_title = title
             else:
+                document_by_crid[allegation.crid] = None
+
                 allegation.document_id = 0
                 allegation.document_normalized_title = ''
                 allegation.document_title = ''
+
             allegation.save()
---
+++ document/tests/test_utils.py
@@ -0,0 +1,20 @@
+from unittest import mock
+from django.core import management
+from django.test.testcases import TestCase
+
+from allegation.factories import AllegationFactory
+
+
+class SendDocumentNotificationTestCase(TestCase):
+    def test_send_notification_on_new_document(self):
+        allegation = AllegationFactory()
+        notification_path = 'common.management.commands.update_documents.send_document_notification'
+        document_cloud_path = 'common.management.commands.update_documents.DocumentCloud'
+        with mock.patch(notification_path) as send_notification:
+            with mock.patch(document_cloud_path) as document_cloud:
+                instance = document_cloud.return_value
+                document = mock.Mock(title="title", id="1-2-3")
+                instance.documents.search.return_value = [document]  # search return result
+
+                management.call_command('update_documents')
+                send_notification.called.should.be.true
