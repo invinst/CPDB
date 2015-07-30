287cab451b2bcb82c9ade1be11be48b443fcbf27
Invisible Cop <dev@invisibleinstitute.com>
2015-07-30 15:18:20 +0700

    [f] 560 - upon clicking the request button
    - DRY code
    
    https://www.pivotaltracker.com/story/show/97078560

--- document/tests/views/ui/test_request_view.py
+++ document/tests/views/ui/test_request_view.py
@@ -16,7 +16,7 @@ class RequestViewTestCase(BaseLiveTestCase):
 
         # the modal should shown
         self.find("#request_modal").is_displayed().should.be.true
-        self.should_see_text("We'll notify you when the document is made available.")
+        self.see_notify_text()
 
         # enter email
         self.find("#request_modal input[name='email']").send_keys(faker.Faker().email())
@@ -25,9 +25,15 @@ class RequestViewTestCase(BaseLiveTestCase):
         # wait until modal hide
         self.until(lambda: not self.find("#request_modal").is_displayed())
         # still see the text as successfully notification
-        self.should_see_text("We'll notify you when the document is made available.")
-        self.find(".complaint-row .btn-request").text.should.equal('Requested')
+        self.see_notify_text()
+        self.check_button_requested()
 
         # check the button text after reload
         self.browser.refresh()
+        self.check_button_requested()
+
+    def see_notify_text(self):
+        self.should_see_text("We'll notify you when the document is made available.")
+
+    def check_button_requested(self):
         self.find(".complaint-row .btn-request").text.should.equal('Requested')
