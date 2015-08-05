1236fc413a3f79648eb31184948a3d6f28558117
Invisible Cop <dev@invisibleinstitute.com>
2015-08-05 15:36:02 +0700

    [r] merge migration

---
+++ common/migrations/0037_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0036_auto_20150805_0809'),
+        ('common', '0036_merge'),
+    ]
+
+    operations = [
+    ]
