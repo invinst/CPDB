57732c5cf0a974ee451ade8b22d678abc3d62ca4
Invisible Scream Cat + Invisible Cop <dev@invisibleinstitute.com>
2015-09-16 11:40:09 +0700

    [r] add _multiprocess_can_split_ True to AllegationApiTestBase

--- allegation/tests/views/base.py
+++ allegation/tests/views/base.py
@@ -4,11 +4,12 @@ from allegation.factories import AllegationFactory
 
 
 class AllegationApiTestBase(TestCase):
+    _multiprocess_can_split_ = True
     _overridden_settings = {
         'ALLEGATION_LIST_ITEM_COUNT': 10,
     }
 
     def setUp(self):
         self.allegations = []
-        for i in range(40):
+        for i in range(26):
             self.allegations.append(AllegationFactory())
