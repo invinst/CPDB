93483abaa5676be80ccdea4744ed0fb27483b0b3
Invisible Golf + Invisible Uniform <dev@invisibleinstitute.com>
2015-10-29 17:33:34 +0700

    [f] 104 - Fine tune header for extra small devices
    https://www.pivotaltracker.com/story/show/106476104

--- allegation/static/allegation/js/components/DataToolPage/Nav.react.js
+++ allegation/static/allegation/js/components/DataToolPage/Nav.react.js
@@ -1,23 +1,51 @@
+var _ = require('lodash');
+var classnames = require('classnames');
+var isMobile = require('ismobilejs');
 var navigate = require('react-mini-router').navigate;
 var React = require('react');
 
+var Base = require('components/Base.react');
 var Logo = require('components/Shared/Logo.react');
 var Search = require('components/Shared/Search.react');
 var SessionStore = require('stores/SessionStore');
 var SiteTitle = require('components/Shared/SiteTitle.react');
+var NavStore = require('stores/NavStore');
 
 
-var Nav = React.createClass({
+var Nav = React.createClass(_.assign(Base(NavStore), {
   render: function() {
+    var mobileExpanded = isMobile.any && this.state.searchExpanded;
+    var leftColClass = classnames(
+      {
+        'col-xs-3': mobileExpanded,
+        'col-xs-11': !mobileExpanded
+      },
+      'col-sm-7'
+    );
+    var rightColClass = classnames(
+      {
+        'col-xs-9': mobileExpanded,
+        'col-xs-1': !mobileExpanded
+      },
+      'col-sm-5'
+    );
+    var navbarClass = classnames(
+      'navbar-collapse',
+      'collapse',
+      {
+        'hidden': mobileExpanded
+      }
+    );
+
     return (
       <div className="navbar navbar-default">
         <div className="container-fluid">
           <div className="row">
-            <div className="col-sm-7">
+            <div className={leftColClass}>
               <div className="navbar-header">
                 <Logo />
               </div>
-              <div className="navbar-collapse collapse" id="navbar-main">
+              <div className={navbarClass} id="navbar-main">
                 <ul className="nav navbar-nav">
                   <li className='site-title'>
                     <SiteTitle changable={true} />
@@ -25,14 +53,14 @@ var Nav = React.createClass({
                 </ul>
               </div>
             </div>
-            <div className="col-sm-5">
-              <Search />
+            <div className={rightColClass}>
+              <Search mobileExpanded={mobileExpanded} />
             </div>
           </div>
         </div>
       </div>
     )
   }
-});
+}));
 
 module.exports = Nav;
--- allegation/static/allegation/js/components/Shared/Search.react.js
+++ allegation/static/allegation/js/components/Shared/Search.react.js
@@ -1,7 +1,10 @@
+var classnames = require('classnames');
+var navigate = require('react-mini-router').navigate;
 var React = require('react');
 
 var AppConstants = require('constants/AppConstants');
 var FilterTagsActions = require('actions/FilterTagsActions');
+var NavActions = require('actions/NavActions');
 
 
 var Search = React.createClass({
@@ -38,18 +41,44 @@ var Search = React.createClass({
     });
   },
 
+  componentDidUpdate: function () {
+    $('#autocomplete').focus();
+  },
+
   select: function (event, ui) {
     event.preventDefault();
     FilterTagsActions.addTag(ui.item.category, ui.item);
     $("#autocomplete").val('');
   },
 
+  onSearchClick: function () {
+    if (!this.props.mobileExpanded) {
+      NavActions.mobileSearchClick();
+    } else {
+      NavActions.mobileSearchCollapse();
+    }
+  },
+
   render: function() {
+    var inputClass = classnames(
+      "ui-autocomplete-input",
+      {
+        'hidden-xs': !this.props.mobileExpanded
+      }
+    );
+    var searchIconClass = classnames(
+      {
+        'glyphicon glyphicon-search': !this.props.mobileExpanded,
+        'glyphicon glyphicon-remove': this.props.mobileExpanded
+      }
+    );
+
     return (
       <form className="navbar-form" role="search">
         <div id="search-wrapper" className="pull-right">
           <input type="text" id="autocomplete" placeholder="Search by name, neighborhood, or complaint"
-                 className="ui-autocomplete-input" autoComplete="off"/>
+                 className={inputClass} autoComplete="off"/>
+          <i onClick={this.onSearchClick} className={searchIconClass}></i>
         </div>
       </form>
     )
