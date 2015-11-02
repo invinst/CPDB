c637795920ab289b083fbd28703f97e9c462abb0
Invisible Golf + Invisible Uniform <dev@invisibleinstitute.com>
2015-11-02 18:13:13 +0700

    [f] 936d - Trigger a filter from gender chart block will not change itself
    [https://www.pivotaltracker.com/story/show/105829660]

--- allegation/views/allegation_race_gender_api.py
+++ allegation/views/allegation_race_gender_api.py
@@ -12,37 +12,67 @@ def return_dict(l, k, v):
     return ret
 
 class AllegationRaceGenderAPI(AllegationAPIView):
+    def __init__(self, *args, **kwargs):
+        super(AllegationRaceGenderAPI, self).__init__(*args, **kwargs)
+        self.ignores = []
+        self.with_filters = []
+        self.without_filters = []
+
     # A bit magical here
     def annotate_count_for(self, items, field):
         return return_dict(list(items.values(field).annotate(count=Count(field))), field, 'count')
 
-    def get(self, request):
-        allegations = self.get_allegations()
-        ignore = [x for x in self.request.GET]
-        allegations_without_filters = self.get_allegations(ignore_filters=ignore)
+    def get_officers(self, allegations):
+        officers_ids = list(allegations.distinct().values_list('officer_id', flat=True))
+        return Officer.objects.filter(pk__in=officers_ids)
 
-        # We need to cast them to list to get out the risk of Django's bad translated SQL
-        # Anyway, this is fucking slow
-        officer_ids = list(allegations.distinct().values_list('officer_id', flat=True))
+    def get_witness(self, allegations):
         crids = list(allegations.distinct().values_list('crid', flat=True))
-        witness = ComplainingWitness.objects.filter(crid__in=crids)
+        return ComplainingWitness.objects.filter(crid__in=crids)
 
-        officers = Officer.objects.filter(pk__in=officer_ids)
-        officer_genders = self.annotate_count_for(officers, 'gender')
-        officer_races = self.annotate_count_for(officers, 'race')
+    def get_allegation_without_filters(self):
+        if len(self.without_filters) == 0:
+            self.without_filters = self.get_allegations()
+        return self.without_filters
 
-        witness = ComplainingWitness.objects.filter(crid__in=crids)
-        witness_genders = self.annotate_count_for(witness, 'gender')
-        witness_races = self.annotate_count_for(witness, 'race')
+    def get_allegation_with_filter(self):
+        if len(self.with_filters) == 0:
+            self.with_filters = self.get_allegations(ignore_filters=self.ignores)
+        return self.with_filters
+
+    def get_allegations_with_ignored_tags(self, filter_tag):
+        if filter_tag not in self.ignores:
+            return self.get_allegation_without_filters()
+        else:
+            return self.get_allegation_with_filter()
+
+    def get_officer_genders(self):
+        allegations = self.get_allegations_with_ignored_tags('officer__gender')
+        return self.annotate_count_for(self.get_officers(allegations), 'gender')
+
+    def get_officer_races(self):
+        allegations = self.get_allegations_with_ignored_tags('officer__race')
+        return self.annotate_count_for(self.get_officers(allegations), 'race')
+
+    def get_witness_genders(self):
+        allegations = self.get_allegations_with_ignored_tags('complainant_gender')
+        return self.annotate_count_for(self.get_witness(allegations), 'gender')
+
+    def get_witness_races(self):
+        allegations = self.get_allegations_with_ignored_tags('complainant_race')
+        return self.annotate_count_for(self.get_witness(allegations), 'race')
+
+    def get(self, request):
+        self.ignores = [x for x in self.request.GET]
 
         data = {
             'officers': {
-                'gender': officer_genders,
-                'race': officer_races
+                'gender': self.get_officer_genders(),
+                'race': self.get_officer_races(),
             },
             'complaining_witness': {
-                'gender': witness_genders,
-                'race': witness_races
+                'gender': self.get_witness_genders(),
+                'race': self.get_witness_races()
             }
         }
 
