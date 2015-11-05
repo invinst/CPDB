0ea53a17722ecb600af02340b001a7872ed793a8
Invisible Scream Cat <dev@invisibleinstitute.com>
2015-11-05 10:26:30 +0700

    [f] 302a - Fill missing date data returned by API
    [https://www.pivotaltracker.com/story/show/105945302]

--- dashboard/static/dashboard/js/utils/NewSessionAnalyticAPI.js
+++ dashboard/static/dashboard/js/utils/NewSessionAnalyticAPI.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var jQuery = require('jquery');
 var moment = require('moment');
 
@@ -5,6 +6,27 @@ var NewSessionPerDayChartActions = require('../actions/OverviewSection/NewSessio
 var AppConstants = require('../constants/AppConstants');
 
 var ajax = null;
+
+// Fill django-rest returned date data
+function fill(data, begin) {
+  index = begin;
+  today = moment().format(AppConstants.DATE_ONLY_FORMAT);
+  filledData = _(data['results']).pluck('created_date').value();
+
+  while (index != today) {
+    index = moment(index).add(1, 'd').format(AppConstants.DATE_ONLY_FORMAT);
+    if (!_(filledData).contains(index)) {
+      data['results'].push({
+        'created_date': index,
+        count: 0
+      });
+    }
+  }
+
+  data['results'] = _(data['results']).sortBy(function(x) { return moment(x['created_date']); }).value();
+
+  return data;
+}
 
 var NewSessionAnalyticAPI = {
   getRecent: function () {
@@ -16,6 +38,7 @@ var NewSessionAnalyticAPI = {
     begin = moment().subtract(AppConstants.NUMBER_OF_DAYS_SHOWN_IN_NEW_SESSION_CHART, 'd').format(AppConstants.DATE_ONLY_FORMAT);
 
     ajax = jQuery.get(AppConstants.NEW_SESSION_ANALYTICS_API_ENDPOINT, {begin: begin, end: end}).done(function (data) {
+      data = fill(data, begin, end);
       NewSessionPerDayChartActions.receivedData(data);
     });
   }
