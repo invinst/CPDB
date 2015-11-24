751863d4bfd0bf2c1e57009b2018ec52b8405965
Invisible Cop+Invisible Wise Monkeys <dev@invisibleinstitute.com>
2015-11-24 12:05:11 +0700

    [r] create merge migrations 0051 and new migration 0052

---
+++ common/migrations/0051_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0046_auto_20151118_0955'),
+        ('common', '0050_reimport_table_category'),
+    ]
+
+    operations = [
+    ]
