f1ca2b7352e8e0d2ed00a74409fbcf54c91f0a3d
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2015-11-24 14:21:55 +0700

    [f] 326 - Mobile site main page
    - Basic search page with animation 
    
    [#108732030] https://www.pivotaltracker.com/story/show/108732030

---
+++ mobile/static/mobile/js/components/MainPage/SearchResultSection.react.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+
+var SearchResultSection = React.createClass({
+  render: function () {
+    return (
+      <div>Search result section</div>
+    )
+  }
+});
+
+module.exports = SearchResultSection;
