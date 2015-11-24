2cc6320751c1c12eff432732b59aed32996b5250
Invisible Cop+Invisible Wise Monkeys <dev@invisibleinstitute.com>
2015-11-24 14:35:50 +0700

    [r] add merge migration 0053

---
+++ common/migrations/0053_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0052_merge'),
+        ('common', '0052_auto_20151124_0504'),
+    ]
+
+    operations = [
+    ]
