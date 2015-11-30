66ddd6e32f966e8168f6c5166f74f4e4cf65883f
Invisible Golf + Invisible Uniform <dev@invisibleinstitute.com>
2015-11-30 15:06:46 +0700

    [r] 884 - session aliasing & search
    - Add missing file
    
    [#108493884] [https://www.pivotaltracker.com/story/show/108493884]

--- dashboard/static/dashboard/js/actions/SessionSection/AddSessionAliasModalActions.js
+++ dashboard/static/dashboard/js/actions/SessionSection/AddSessionAliasModalActions.js
@@ -1,32 +1,32 @@
 var AppDispatcher = require('../../dispatcher/AppDispatcher');
 var AppConstants = require('../../constants/AppConstants');
-var AliasAPI = require('../../utils/AliasAPI');
+var SessionAliasAPI = require('../../utils/SessionAliasAPI');
 
-var AddAliasModalActions = {
+var AddSessionAliasModalActions = {
   show: function (data) {
     AppDispatcher.dispatch({
-      actionType: AppConstants.SHOW_ADD_ALIAS_MODAL,
+      actionType: AppConstants.SHOW_ADD_SESSION_ALIAS_MODAL,
       data: data
     });
   },
 
   hide: function () {
     AppDispatcher.dispatch({
-      actionType: AppConstants.HIDE_ADD_ALIAS_MODAL
+      actionType: AppConstants.HIDE_ADD_SESSION_ALIAS_MODAL
     });
   },
 
   createAlias: function(alias, target) {
-    AliasAPI.create(alias, target);
+    SessionAliasAPI.create(alias, target);
   },
 
   formDataChange: function(stateName, stateValue) {
     AppDispatcher.dispatch({
-      actionType: AppConstants.ALIAS_MODAL_FORM_DATA_CHANGED,
+      actionType: AppConstants.SESSION_ALIAS_MODAL_FORM_DATA_CHANGED,
       stateName: stateName,
       stateValue: stateValue
     });
   }
 };
 
-module.exports = AddAliasModalActions;
+module.exports = AddSessionAliasModalActions;
