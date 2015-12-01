6d9ee3248dd83afed640b1fbc66db4332fd2302c
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2015-12-01 18:13:04 +0700

    [f] 324a - Add RelatedOfficerService
    [#109227980] https://www.pivotaltracker.com/story/show/109227980

--- mobile/urls.py
+++ mobile/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from mobile.views.mobile_site_view import MobileSiteView
 from mobile.views.mobile_suggestion_view import MobileSuggestionView
 
+
 urlpatterns = [
     url(r'^$', MobileSiteView.as_view(), name='home'),
     url(r'^api/suggestion/$', MobileSuggestionView.as_view(), name='suggestion'),
