dc362f3c516027fde578f0e2a8a0c912cbe61841
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2015-12-01 18:15:08 +0700

    [r] Refactor serializers

--- mobile/serializers/mobile_suggestion_serializer.py
+++ mobile/serializers/mobile_suggestion_serializer.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 
-from common.models import Officer
 from mobile.serializers.allegation_serializer import AllegationSerializer
 from mobile.serializers.officer_serializer import OfficerSerializer
 
@@ -9,13 +8,3 @@ class MobileSuggestionSerializer(serializers.Serializer):
     allegation = AllegationSerializer()
     officer_by_star = OfficerSerializer()
     officers_by_name = OfficerSerializer(many=True)
-
-    class Meta:
-        model = Officer
-        fields = (
-            'allegation',
-            'officer_by_star',
-            'officers_by_name',
-            'race',
-            'gender'
-        )
