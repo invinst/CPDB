acffbb5ae403905ed643d916ed0df1371f11f83d
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2015-12-01 19:11:54 +0700

    [r] Refactor BadRequestApiException

--- mobile/views/mobile_related_officers_view.py
+++ mobile/views/mobile_related_officers_view.py
@@ -1,17 +1,11 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.exceptions import APIException
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
 from common.models import Officer
+from mobile.exceptions.bad_request_api_exception import BadRequestApiException
 from mobile.serializers.mobile_related_officer_view_seriallizer import MobileRelatedOfficerViewSerializer
 from mobile.services.related_officer_service import RelatedOfficerService
-
-
-class BadRequestException(APIException):
-    status_code = HTTP_400_BAD_REQUEST
-    default_detail = 'Bad request'
 
 
 class MobileRelatedOfficersView(APIView):
@@ -21,7 +15,7 @@ class MobileRelatedOfficersView(APIView):
         try:
             officer = get_object_or_404(Officer, pk=pk)
         except ValueError:
-            raise BadRequestException
+            raise BadRequestApiException
 
         co_accused_officers = RelatedOfficerService.co_accused_officers(officer.pk)
         witness_officers = RelatedOfficerService.witness_officers(officer.pk)
