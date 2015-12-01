e8175d40c412678a054080df1438db87c3a3b4ed
Invisible Golf <dev@invisibleinstitute.com>
2015-12-01 10:52:20 +0700

    [b] 728 - why did the number of total allegations in the the sunburst summary text go from 56,370 to 56,362
    - update to the fix final outcome and final finding script to zero pad outcomes
    
    [#109214728] [https://www.pivotaltracker.com/story/show/109214728]

--- common/management/commands/correct_allegation_outcome_finding.py
+++ common/management/commands/correct_allegation_outcome_finding.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from common.models import Allegation
+from common.models import Allegation, OUTCOMES
 
 
 class Command(BaseCommand):
@@ -11,3 +11,8 @@ class Command(BaseCommand):
         Allegation.objects.filter(final_outcome='0').update(final_outcome='000')
         Allegation.objects.filter(final_outcome='15').update(final_outcome='015')
         Allegation.objects.filter(final_outcome='999').update(final_outcome=None)
+
+        for outcome in OUTCOMES:
+            if outcome[0] and len(outcome[0]) < 3:
+                update = outcome[0].zfill(3)
+                Allegation.objects.filter(final_outcome=outcome[0]).update(final_outcome=update)
