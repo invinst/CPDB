4107a7ba65c59df9b0ecc7bb5802b42521e5d1ca
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2015-12-02 14:50:05 +0700

    [f] 330a - Add complaining_witness info to API
    [#109324522] https://www.pivotaltracker.com/story/show/109324522

--- mobile/urls.py
+++ mobile/urls.py
@@ -8,7 +8,6 @@ from mobile.views.mobile_related_officers_view import MobileRelatedOfficersView
 from mobile.views.mobile_site_view import MobileSiteView
 from mobile.views.mobile_suggestion_view import MobileSuggestionView
 
-
 urlpatterns = [
     url(r'^$', MobileSiteView.as_view(), name='home'),
     url(r'^api/suggestion/$', MobileSuggestionView.as_view(), name='suggestion'),
