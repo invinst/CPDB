bb404255e1f20e33142738643b933adc60ec66ac
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2015-12-02 10:04:00 +0700

    [f] 324a - API for getting mobile officer details
    [#109227980] https://www.pivotaltracker.com/story/show/109227980

--- mobile/urls.py
+++ mobile/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from mobile.views.mobile_officer_allegation_view import MobileOfficerAllegationView
+from mobile.views.mobile_officer_view import MobileOfficerView
 from mobile.views.mobile_related_officers_view import MobileRelatedOfficersView
 from mobile.views.mobile_site_view import MobileSiteView
 from mobile.views.mobile_suggestion_view import MobileSuggestionView
@@ -9,6 +10,7 @@ from mobile.views.mobile_suggestion_view import MobileSuggestionView
 urlpatterns = [
     url(r'^$', MobileSiteView.as_view(), name='home'),
     url(r'^api/suggestion/$', MobileSuggestionView.as_view(), name='suggestion'),
+    url(r'^api/officer/$', MobileOfficerView.as_view(), name='officer'),
     url(r'^api/officer/allegation/$', MobileOfficerAllegationView.as_view(), name='officer-allegation'),
     url(r'^api/officer/related_officer/$', MobileRelatedOfficersView.as_view(), name='officer-related_officer'),
 ]
