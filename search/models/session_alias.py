188b4dbdeda3aa7228c76c1680c24ca0888d350a
Invisible Cop+Invisible Uniform <dev@invisibleinstitute.com>
2015-12-02 09:21:05 +0700

    [f] 884 - sessions alias admin
    [#108493884] [https://www.pivotaltracker.com/story/show/108493884]

--- search/models/session_alias.py
+++ search/models/session_alias.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from share.models import Session
 
@@ -6,3 +7,6 @@ from share.models import Session
 class SessionAlias(models.Model):
     alias = models.CharField(max_length=254)
     session = models.ForeignKey(Session)
+
+    class Meta:
+        verbose_name_plural = _('aliases')
