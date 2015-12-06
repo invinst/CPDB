1b2d935d62356a8666dcb63dbc9ce1b5162e8824
Invisible Cop <dev@invisibleinstitute.com>
2015-12-06 14:13:38 +0000

    [r] add areas__id to test_readable_query

--- share/tests/models/test_session_model.py
+++ share/tests/models/test_session_model.py
@@ -1,4 +1,4 @@
-from allegation.factories import InvestigatorFactory
+from allegation.factories import InvestigatorFactory, AreaFactory
 from common.tests.core import SimpleTestCase
 from share.factories import SessionFactory
 
@@ -8,11 +8,15 @@ class SessionModelTestCase(SimpleTestCase):
         self.session = SessionFactory()
 
     def test_readable_query(self):
+        area = AreaFactory()
         investigator = InvestigatorFactory()
         self.session.query = {
             'filters': {
                 'investigator': {
                     'value': [investigator.id],
+                },
+                'areas__id': {
+                    'value': [area.id],
                 },
             },
         }
@@ -21,5 +25,9 @@ class SessionModelTestCase(SimpleTestCase):
             'investigator': [{
                 'value': investigator.id,
                 'text': investigator.name,
+            }],
+            'areas__id': [{
+                'text': "%s: %s" % (area.type, area.name),
+                'value': area.id
             }]
         })
