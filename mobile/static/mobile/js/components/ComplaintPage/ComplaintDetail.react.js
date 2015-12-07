814ff31502d05c4ceadf3a439966c01f0234f408
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2015-12-07 13:45:37 +0700

    WIP

--- mobile/static/mobile/js/components/ComplaintPage/ComplaintDetail.react.js
+++ mobile/static/mobile/js/components/ComplaintPage/ComplaintDetail.react.js
@@ -3,36 +3,26 @@ var Separator = require('components/Shared/Separator.react');
 
 var ComplaintDetail = React.createClass({
   render: function () {
+    var info = this.props.info;
+    
     return (
       <div className='complaint-detail'>
-        <div className='complaint-category'>
-          Arrest/Look-up Procedures
+        <div className='headline'>
+          <span className='crid-info inline-block half-width align-left'>
+            <span className='crid-title'>CRID</span>
+            <span className='crid-number'>{info.crid}</span>
+          </span>
+          <span className='final-finding inline-block half-width align-right'>
+            {info.final_finding}
+          </span>
+        </div>
+        <div className='complaint-category bold'>
+          {info.cat.category}
         </div>
         <div className='complaint-sub-category'>
-          03C Search Of Premise/Vehicle Without Warrant
+          {info.cat.allegation_name}
         </div>
-        <div className='detail'>
-          <div className='under-line'>
-            <div className='inline'>
-              <label>CRID</label><span>104909</span>
-            </div>
-            <Separator />
-            <div className='inline'><label>Incident Date</label><span>2014-10-23</span>
-            </div>
-          </div>
-          <div className='under-line'>
-            <label>Finding</label>
-            <span>Open investigation</span>
-          </div>
-          <div className='under-line'>
-            <label>Outcome</label>
-            <span>No action taken</span>
-          </div>
-          <div>
-            <label>Complaining Witness</label>
-            <span>Black, Female, Age 25</span>
-          </div>
-        </div>
+        <a href='#' className='document-link'>View documents</a>
       </div>
     )
   }
--- mobile/static/mobile/js/components/ComplaintPage/InvestigationTimeline.react.js
+++ mobile/static/mobile/js/components/ComplaintPage/InvestigationTimeline.react.js
@@ -7,10 +7,6 @@ var InvestigationTimeline = React.createClass({
   render: function () {
     return (
       <div className='investigation-timeline'>
-        <div className='section-header'>
-          <span className='section-title'>Investigation timeline</span>
-        </div>
-        <SimpleTimeline />
       </div>
     )
   }
