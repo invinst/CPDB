c3ab4b16da85089288fb090d506b5b0f912a4ebb
Invisible Cop+Invisible Uniform <dev@invisibleinstitute.com>
2015-12-08 14:45:57 +0700

    [r] add merge migration share/0013

---
+++ share/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('share', '0012_auto_20151201_0314'),
+        ('share', '0010_session_sunburst_arc'),
+    ]
+
+    operations = [
+    ]
