5303717b76f9d48b2bbae93ff6887bc993600284
Invisible Alfa <dev@invisibleinstitute.com>
2015-12-16 16:40:32 +0700

    [r] temporary fix merging conflict

---
+++ common/migrations/0057_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0056_pending_pdf_allegation'),
+        ('common', '0055_auto_20151216_0316'),
+    ]
+
+    operations = [
+    ]
