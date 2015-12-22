dd771a7612655023fb61d6b8aab78462e45ba1ab
Invisible Wise Monkeys+Invisible Passion <dev@invisibleinstitute.com>
2015-12-22 14:20:16 +0700

    [r] resolve migrations conflict

--- common/migrations/0056_merge.py
+++ common/migrations/0056_merge.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('common', '0056_pending_pdf_allegation'),
+        ('common', '0055_pending_pdf_allegation_and_allegation_summary'),
         ('common', '0055_auto_20151216_0316'),
     ]
 
