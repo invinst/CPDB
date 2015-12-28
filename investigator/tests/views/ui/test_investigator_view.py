ec9680a340b55ee5ab1ed8bad9e58683989b4458
Invisible Wise Monkeys+Invisible Passion <dev@invisibleinstitute.com>
2015-12-28 09:53:04 +0700

    [r] 896 - fix failing test
    [#99184896] https://www.pivotaltracker.com/story/show/99184896

--- investigator/tests/views/ui/test_investigator_view.py
+++ investigator/tests/views/ui/test_investigator_view.py
@@ -15,6 +15,6 @@ class InvestigatorViewTestCase(BaseLiveTestCase):
         self.until(lambda: self.element_by_classname_and_text('investigator-name', self.investigator.name))
         # click by javascript to avoid random failed
         self.browser.execute_script('jQuery(".investigator-name").click()')
-        self.until(lambda: self.should_see_text(self.investigator.name.upper()))
+        self.until(lambda: self.should_see_text(self.investigator.name))
         len(self.find_all('.complaint-row')).should.equal(1)
         self.should_see_text(self.allegation.cat.allegation_name)
