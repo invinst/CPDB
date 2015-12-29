7578abf7fd9ab2cf88e8aec50d54546890e4a708
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2015-12-29 11:10:03 +0700

    [f] Add icon search/close to the search bar

--- mobile/static/mobile/js/components/Shared/SearchBar.react.js
+++ mobile/static/mobile/js/components/Shared/SearchBar.react.js
@@ -1,12 +1,30 @@
 var React = require('react');
+var cx = require('classnames');
+
+var HelperUtil = require('utils/HelperUtil');
+
 
 var SearchBar = React.createClass({
+  getInitialState: function () {
+    return {
+      'searchState': 'search'
+    }
+  },
+
   onInputChange: function () {
+    this.setState({
+      'searchState': 'close'
+    });
   },
 
   render: function () {
+    var leftIconClassName = cx('icon', HelperUtil.format('icon-{searchState}', {'searchState': this.state.searchState}));
+
     return (
-      <input className='search-bar' placeholder='Search officers or complaints' onChange={this.onInputChange} />
+      <div className='search-bar'>
+        <input className='input-text' placeholder='Search officers or complaints' onChange={this.onInputChange}/>
+        <span className={leftIconClassName}></span>
+      </div>
     );
   }
 });
