614e6ea9f47b52854f17fe54ee5c8c0bc9c6af0d
Invisible Alfa <dev@invisibleinstitute.com>
2015-12-30 10:50:24 +0700

    [f] 982 - check if addresses match up with the beat numbers
    - add export function
    
    [#106041982] [https://www.pivotaltracker.com/story/show/106041982]

--- common/management/commands/check_beat_and_point.py
+++ common/management/commands/check_beat_and_point.py
@@ -1,3 +1,4 @@
+import csv
 from common.models import Allegation
 from django.core.management.base import BaseCommand
 from django.db.models.query_utils import Q
@@ -6,21 +7,31 @@ from django.db.models.query_utils import Q
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        q = Q(Q(beat=None) | Q(point=None))
+        q = Q(beat=None) | Q(point=None)
         counter = {
             'fail': 0,
             'success': 0
         }
-        for allegation in Allegation.objects.all().exclude(q):
-            if not allegation.beat.polygon.contains(allegation.point):
-                distance = allegation.beat.polygon.distance(allegation.point) * 100
-                if distance > 100:
-                    print("{crid}'s point is not within its beat {beat} and {distance}m away".format(
-                        crid=allegation.crid,
-                        beat=allegation.beat.name,
-                        distance=distance
-                    ))
-                    counter['fail'] += 1
-            else:
-                counter['success'] += 1
-        print(counter)
+        with open('beat_mismatch.csv', 'w') as f:
+            writer = csv.writer(f)
+
+            for allegation in Allegation.objects.all().exclude(q):
+                if not allegation.beat.polygon.contains(allegation.point):
+                    distance = allegation.beat.polygon.distance(allegation.point) * 100
+                    if distance > 10:
+                        print("{crid}'s point is not within its beat {beat} and {distance}m away".format(
+                            crid=allegation.crid,
+                            beat=allegation.beat.name,
+                            distance=distance
+                        ))
+                        counter['fail'] += 1
+                        writer.writerow([
+                            allegation.crid,
+                            allegation.incident_date,
+                            allegation.point.x,
+                            allegation.point.y,
+                            distance
+                        ])
+                else:
+                    counter['success'] += 1
+            print(counter)
