6397522d7464aa9f5c59f9528544f5e06666a0f5
Invisible Golf <dev@invisibleinstitute.com>
2016-01-05 12:30:02 +0700

    [f] 798 - onHover “?” text for the sustained summary text box in new expanded allegation view
    - Added integration test
    
    [#110737798] [https://www.pivotaltracker.com/story/show/110737798]

---
+++ dashboard/tests/integrations/test_interfacetext.py
@@ -0,0 +1,30 @@
+from common.tests.core import BaseAdminTestCase
+from api.models import Setting, InterfaceText
+from officer.factories import StoryFactory
+
+
+class InterfaceTextEditTestCase(BaseAdminTestCase):
+    def setUp(self):
+        super(InterfaceTextEditTestCase, self).setUp()
+
+
+        self.go_to_section('Interface Texts')
+
+
+    def test_update_interface_text(self):
+        # Default Site Title
+        self.find(".fa-pencil").click()
+
+        self.until(lambda: self.find('#id_text'))
+
+        input_field = self.find("#id_text")
+
+        add_text = "Updating the summary text field"
+        input_field.send_keys(add_text)
+
+        self.find(".fa-floppy-o").click()
+        self.until(self.ajax_complete)
+        self.should_see_text("Updating the summary text field")
+
+        InterfaceText.objects.filter(text__contains=add_text).count().should.equal(1)
+
