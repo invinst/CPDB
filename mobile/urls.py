a328688662823b0aed50e6bf9dc10573a79d806f
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2016-01-08 15:07:37 +0700

    [f] 734 - Cache mobile views
    - Remove test page

--- mobile/urls.py
+++ mobile/urls.py
@@ -1,26 +1,29 @@
 from django.conf.urls import url, include
 from django.views.decorators.csrf import ensure_csrf_cookie
 
+from common.middleware.cache import orderless_cache_page
 from mobile.views.mobile_allegation_view import MobileAllegationView
 from mobile.views.mobile_officer_view import MobileOfficerView
 from mobile.views.mobile_site_view import MobileSiteView
 from mobile.views.mobile_suggestion_view import MobileSuggestionView
 
+
+cache_view = orderless_cache_page(86400 * 90)
+
 urlpatterns = [
-    url(r'^$', MobileSiteView.as_view(), name='home'),
+    url(r'^$', cache_view(MobileSiteView.as_view()), name='home'),
     url(r'^lookup/', include('common.urls', namespace='common')),
 
     # api urls
-    url(r'^api/suggestion/$', MobileSuggestionView.as_view(), name='suggestion'),
-    url(r'^api/allegation/$', MobileAllegationView.as_view(), name='allegation'),
-    url(r'^api/officer/$', MobileOfficerView.as_view(), name='officer'),
+    url(r'^api/suggestion/$', cache_view(MobileSuggestionView.as_view()), name='suggestion'),
+    url(r'^api/allegations/$', cache_view(MobileAllegationView.as_view()), name='allegation'),
+    url(r'^api/officer/$', cache_view(MobileOfficerView.as_view()), name='officer'),
 
     # fall-back urls
-    url(r'^mobile/api/suggestion/$', MobileSuggestionView.as_view(), name='mobile-suggestion'),
-    url(r'^mobile/api/allegation/$', MobileAllegationView.as_view(), name='mobile-allegation'),
-    url(r'^mobile/api/officer/$', MobileOfficerView.as_view(), name='mobile-officer'),
+    url(r'^mobile/api/suggestion/$', cache_view(MobileSuggestionView.as_view()), name='mobile-suggestion'),
+    url(r'^mobile/api/allegation/$', cache_view(MobileAllegationView.as_view()), name='mobile-allegation'),
+    url(r'^mobile/api/officer/$', cache_view(MobileOfficerView.as_view()), name='mobile-officer'),
 
     # overriding for client-side routing
-    url(r'^(officer/[^/]+/\d+|complaint/\d+|search/\w+)?$', ensure_csrf_cookie(MobileSiteView.as_view()), name='home'),
-    url(r'^(test)?$', ensure_csrf_cookie(MobileSiteView.as_view()), name='home'),
+    url(r'^(officer/[^/]+/\d+|complaint/\d+|search/\w+)?$', cache_view(MobileSiteView.as_view()), name='home'),
 ]
