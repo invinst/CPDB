2e1d6844abec72f9ae436a4eaaca5c0170b2fb01
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2016-01-14 17:21:32 +0700

    [r] Fix coding style

--- mobile/tests/integrations/test_mobile_complaint_document_link.py
+++ mobile/tests/integrations/test_mobile_complaint_document_link.py
@@ -1,6 +1,6 @@
-
 from allegation.factories import AllegationFactory, OfficerAllegationFactory
 from common.tests.core import BaseLivePhoneTestCase, BaseLiveAndroidPhoneTestCase
+
 
 class MobileComplaintDocumentTestMixin():
     def go_to_allegation_detail_page(self, crid=''):
@@ -9,8 +9,10 @@ class MobileComplaintDocumentTestMixin():
     def setUp(self):
         document_id = '1678325'
         document_normalized_title = 'cr-1059875'
-        self.allegation = AllegationFactory(document_normalized_title=document_normalized_title, document_id=document_id)
+        self.allegation = AllegationFactory(document_normalized_title=document_normalized_title,
+                                            document_id=document_id)
         OfficerAllegationFactory(allegation=self.allegation)
+
 
 class MobileComplaintDocumentAndroidTest(MobileComplaintDocumentTestMixin, BaseLiveAndroidPhoneTestCase):
     def test_view_cloud_pdf(self):
@@ -20,7 +22,6 @@ class MobileComplaintDocumentAndroidTest(MobileComplaintDocumentTestMixin, BaseL
 
 
 class MobileComplaintDocumentIOSTest(MobileComplaintDocumentTestMixin, BaseLivePhoneTestCase):
-
     def test_view_pdf_directly(self):
         view_document_link = 'http://documentcloud.org/documents/1678325-cr-1059875.pdf'
         self.go_to_allegation_detail_page(crid=self.allegation.crid)
