d16328c81ea2afa4b0d94388fb98cc8b403e4054
Invisible Scream Cat+Invisible Tango <dev@invisibleinstitute.com>
2016-01-14 17:33:08 +0700

    [r] Fix `;`

--- mobile/static/mobile/js/components/ComplaintPage/DocumentLink.react.js
+++ mobile/static/mobile/js/components/ComplaintPage/DocumentLink.react.js
@@ -11,7 +11,8 @@ var DocumentLink = React.createClass({
 
     if (deviceUtil.isiOSDevice) {
       return presenter.pdfLink;
-    }
+    };
+
     return presenter.cloudLink;
   },
 
@@ -21,7 +22,7 @@ var DocumentLink = React.createClass({
 
     if (!documentId) {
       return (<div></div>);
-    }
+    };
 
     var documentLink = this.getDocumentLink(documentId, documentNormalizedTitle);
     return (
