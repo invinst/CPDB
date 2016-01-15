533ee6598ef317fea6dd61a97df3115d5780d7d9
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-01-15 10:49:54 +0700

    [f] 192 - Implement twitter button
    [#107657192] https://www.pivotaltracker.com/story/show/107657192

--- allegation/static/allegation/js/components/DataToolPage/Share/ShareBar.react.js
+++ allegation/static/allegation/js/components/DataToolPage/Share/ShareBar.react.js
@@ -1,16 +1,27 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 
 var ShareBar = React.createClass({
-  inputClicked: function() {
+  inputClicked: function () {
     this.refs.urlText.select();
+  },
+
+  getTweetIntentUrl: function () {
+    return [
+      'https://twitter.com/intent/tweet',
+      '?text=Check+out',
+      '&url=' + encodeURIComponent(window.location.href),
+    ].join('');
   },
 
   render: function () {
     return <div className="share-bar">
       <div className="share-bar__content-wrapper">
         <i className="fa fa-facebook-square"></i>
-        <i className="fa fa-twitter-square"></i>
+        <a href={this.getTweetIntentUrl()}>
+          <i className="fa fa-twitter-square"></i>
+        </a>
         <input type="text" value={window.location.href}
           readonly onClick={this.inputClicked} ref="urlText"/>
       </div>
