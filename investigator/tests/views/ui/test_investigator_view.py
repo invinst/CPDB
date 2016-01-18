07a0cadabac04d3bb0de2191325ed704944b696a
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-01-18 16:08:19 +0700

    [b] 970 - wrong highlight in investigator page     - add test for highlight case     - resolve multiple expand when click on complaint row [#111807970] https://www.pivotaltracker.com/story/show/111807970

--- investigator/tests/views/ui/test_investigator_view.py
+++ investigator/tests/views/ui/test_investigator_view.py
@@ -8,7 +8,10 @@ class InvestigatorViewTestCase(BaseLiveTestCase):
         self.investigator = InvestigatorFactory()
         allegation = AllegationFactory(investigator=self.investigator)
         self.officer_allegation = \
-            OfficerAllegationFactory(allegation=allegation)
+            OfficerAllegationFactory(
+                allegation=allegation,
+                final_outcome_class='disciplined'
+                )
 
     def test_browse_investigator_page(self):
         self.visit_home()
@@ -21,3 +24,35 @@ class InvestigatorViewTestCase(BaseLiveTestCase):
         self.until(lambda: self.should_see_text(self.investigator.name))
         len(self.find_all('.complaint-row')).should.equal(1)
         self.should_see_text(self.officer_allegation.cat.allegation_name)
+
+    def test_highlight_disciplined_row(self):
+        self.visit('/investigator/sluglify/{id}'.format(
+            id=self.investigator.id
+            ))
+        # Check discipline complaint row was highlighted
+        self.element_exist('.complaint-row.disciplined').should.be.true
+
+    def test_expand_complaint_row(self):
+        OfficerAllegationFactory(
+            allegation=self.officer_allegation.allegation,
+            final_outcome_class='other'
+            )
+        self.visit('/investigator/sluglify/{id}'.format(
+            id=self.investigator.id
+            ))
+
+        # Check two complaint rows exist
+        self.element_exist('.complaint-row.other')
+        self.element_exist('.complaint-row.disciplined')
+
+        # Expand one row
+        self.find('.complaint-row.disciplined').click()
+
+        # Make sure it expand above target
+        self.element_exist(
+            '.complaint-row.disciplined .complaint_detail.closed'
+            ).should.be.false
+        # And don't expand another
+        self.element_exist(
+            '.complaint-row.other .complaint_detail'
+            ).should.be.false
