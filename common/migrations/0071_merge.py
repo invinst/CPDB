88d2d741688d36ab1473c44152d6d66fef97a559
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-01-20 14:36:45 +0700

    [r] Merge `0070_auto_20160119_0516` with `0069_add_timestamp_fields` to resolve migration conflict

---
+++ common/migrations/0071_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0070_auto_20160119_0516'),
+        ('common', '0069_add_timestamp_fields'),
+    ]
+
+    operations = [
+    ]
