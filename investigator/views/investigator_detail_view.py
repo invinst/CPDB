26fe706e7e316c79987bee4870376d84c6a8adc5
Invisible Golf+Invisible Uniform <dev@invisibleinstitute.com>
2016-01-22 10:36:34 +0700

    - Fixing imports

--- investigator/views/investigator_detail_view.py
+++ investigator/views/investigator_detail_view.py
@@ -1,7 +1,7 @@
 from django.views.generic.base import View
 from django.shortcuts import get_object_or_404
 
-from common.models import Investigator, OfficerAllegation
+from common.models import Investigator
 from document.response import JsonResponse
 from investigator.services.investigator_details_service import \
     InvestigatorDetailsService
