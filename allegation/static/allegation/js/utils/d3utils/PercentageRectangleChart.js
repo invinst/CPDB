70ed7798432e33f324254e438bf352480d21e174
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-01-27 14:30:28 +0700

    [r] make the percentages add up to 100%

--- allegation/static/allegation/js/utils/d3utils/PercentageRectangleChart.js
+++ allegation/static/allegation/js/utils/d3utils/PercentageRectangleChart.js
@@ -1,12 +1,25 @@
 var d3 = require('d3');
 var _ = require('lodash');
 
+function calculatePercentages (l) {
+  var sum = _.reduce(l, function (acc, x) { return acc + x.value }, 0);
+  var off = 100 - _.reduce(l, function(acc, x) { return acc + Math.floor(x.value*100/sum) }, 0);
+  return _.chain(l)
+          .sortBy(function(x) { return Math.floor(x.value*100/sum) - (x.value*100/sum) })
+          .map(function(x, i) {
+            x.percent = Math.floor(x.value*100/sum) + (off >= l.length - i);
+            return x;
+          }).value();
+}
+
+
 var PercentageRectangleChart = {
   draw: function(data, options, domCSSPath, clickHandler) {
     // TODO: defaultOptions
     var colors = options.colors;
     var width = options.width;
     var height = options.height;
+    var data = calculatePercentages(data);
 
     var currentY = 0;
     var values = _.pluck(data, 'value');
@@ -58,11 +71,7 @@ var PercentageRectangleChart = {
       .attr('x', 20)
       .attr('y', function(d) { return heightScale(d.value) / 2 + minHeight - 5})
       .text(function(d, i) {
-        var percent = d.value * 100 / sum;
-        if (percent < 1) {
-          percent = 1;
-        }
-        return d.label + ' ' + (Math.floor(percent)) + '%';
+        return d.label + ' ' + d.percent + '%';
       })
       .on("click", clickHandler);
   }
