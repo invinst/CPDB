bb00fbd5b7a8dd012dee578a7c454c4ae16e1f2a
Invisible Golf+Invisible Uniform <dev@invisibleinstitute.com>
2016-02-04 10:24:32 +0700

    - first migrate because of taggit, then remove, then migrate

--- pre_deploy.sh
+++ pre_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin.bash
 
 echo "SELECT pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where datname = 'cpdb_staging’;" > psql cpdb; dropdb cpdb_staging; createdb cpdb_staging; pg_dump cpdb | psql cpdb_staging;
+./manage.py migrate
 echo "DELETE FROM django_migrations WHERE app in ('common', 'officer', 'api');"
 ./manage.py migrate common 0001 --fake
 ./manage.py migrate officer 0001 --fake
