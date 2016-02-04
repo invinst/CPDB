d4db560962a3de9113996e91daa518d4bf7bb048
Invisible Golf+Invisible Uniform <dev@invisibleinstitute.com>
2016-02-04 10:50:21 +0700

    - Removed staging specific pre_deploy command

--- pre_deploy.sh
+++ pre_deploy.sh
@@ -1,7 +1,5 @@
 #!/bin.bash
 
-echo "SELECT pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where datname = 'cpdb_staging’;" > psql cpdb; dropdb cpdb_staging; createdb cpdb_staging; pg_dump cpdb | psql cpdb_staging;
-./manage.py migrate
 echo "DELETE FROM django_migrations WHERE app in ('common', 'officer', 'api');" | psql cpdb_staging
 ./manage.py migrate common 0001 --fake
 ./manage.py migrate officer 0001 --fake
