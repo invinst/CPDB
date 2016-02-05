2cb845811c043a35be558f3f2edb0932608efe65
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-02-05 10:38:32 +0700

    [b] 939 - race/gender chart have unstable color/order
    [#113138939] https://www.pivotaltracker.com/story/show/113138939

--- allegation/static/allegation/js/utils/d3utils/PercentageRectangleChart.js
+++ allegation/static/allegation/js/utils/d3utils/PercentageRectangleChart.js
@@ -5,11 +5,14 @@ function calculatePercentages(l) {
   var sum = _.reduce(l, function (acc, x) { return acc + x.value; }, 0);
   var off = 100 - _.reduce(l, function (acc, x) { return acc + Math.floor(x.value*100/sum); }, 0);
   return _.chain(l)
+          .map(function (x, i) { x.oldIndex = i; return x; })
           .sortBy(function (x) { return Math.floor(x.value*100/sum) - (x.value*100/sum); })
           .map(function (x, i) {
             x.percent = Math.floor(x.value*100/sum) + (off >= l.length - i);
             return x;
-          }).value();
+          })
+          .sortBy(function (x) { return x.oldIndex; })
+          .value();
 }
 
 
