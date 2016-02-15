6cb1e3fb6ee6a3a761049dd2d4052b7197075e4b
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-02-15 16:10:33 +0700

    [b] 939 - Add client side tests
    [#113138939] https://www.pivotaltracker.com/story/show/113138939

--- allegation/static/allegation/js/utils/d3utils/PercentageRectangleChart.js
+++ allegation/static/allegation/js/utils/d3utils/PercentageRectangleChart.js
@@ -1,19 +1,6 @@
 var d3 = require('d3');
 var _ = require('lodash');
-
-function calculatePercentages(l) {
-  var sum = _.reduce(l, function (acc, x) { return acc + x.value; }, 0);
-  var off = 100 - _.reduce(l, function (acc, x) { return acc + Math.floor(x.value*100/sum); }, 0);
-  return _.chain(l)
-          .map(function (x, i) { x.oldIndex = i; return x; })
-          .sortBy(function (x) { return Math.floor(x.value*100/sum) - (x.value*100/sum); })
-          .map(function (x, i) {
-            x.percent = Math.floor(x.value*100/sum) + (off >= l.length - i);
-            return x;
-          })
-          .sortBy(function (x) { return x.oldIndex; })
-          .value();
-}
+var calculatePercentages = require('utils/calculatePercentages');
 
 
 var PercentageRectangleChart = {
