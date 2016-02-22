03339c9e751c399428873d9e3b99ab45240eb373
Invisible Golf+Invisible Uniform <dev@invisibleinstitute.com>
2016-02-22 10:21:28 +0700

    [f] 927 - twitter bot
    - Squash migrations
    
    [#113139927] [https://www.pivotaltracker.com/story/show/113139927]

--- twitterbot/migrations/0001_initial.py
+++ twitterbot/migrations/0001_initial.py
@@ -11,18 +11,28 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Response',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('type', models.CharField(choices=[['officer', 'officer'], ['investigator', 'investigator'], ['not_found', 'not_found']], max_length=20)),
+                ('message', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
             name='TwitterResponse',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('created_at', models.DateTimeField(auto_created=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('response', models.TextField()),
+                ('user', models.CharField(default='', max_length=50)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
         ),
         migrations.CreateModel(
             name='TwitterSearch',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('refresh_url', models.CharField(max_length=255)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('query', models.CharField(max_length=100)),
+                ('refresh_url', models.CharField(blank=True, null=True, max_length=255)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
             ],
         ),
