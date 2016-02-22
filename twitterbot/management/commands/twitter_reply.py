7420d6cb9f288aaa86650c483c3647b8f0161cdf
Invisible Golf <dev@invisibleinstitute.com>
2016-02-22 17:27:51 +0700

    [f] 791 - follow links in tweets that @mention us
    - regex urls in tweets
    - download html, strip away anything, search remaining text for officer names
    
    [#113764791] [https://www.pivotaltracker.com/story/show/113764791]

--- twitterbot/management/commands/twitter_reply.py
+++ twitterbot/management/commands/twitter_reply.py
@@ -1,6 +1,10 @@
+from bs4 import BeautifulSoup
+import re
+
 from django.core.management.base import BaseCommand
 
 from common.models import Officer, Investigator
+import requests
 from search.services.suggest.suggest_investigator import SuggestInvestigator
 from search.services.suggest.suggest_officer import SuggestOfficerName
 from twitterbot.models import TwitterSearch, Response, TwitterResponse
@@ -8,6 +12,7 @@ from twitterbot.models import TwitterSearch, Response, TwitterResponse
 
 class Command(BaseCommand):
     help = 'Post Twitter replies'
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
     @classmethod
     def create_responses(cls, try_officer_name):
@@ -29,33 +34,60 @@ class Command(BaseCommand):
 
         return responses
 
+    def handle_urls(self, urls, status, search):
+
+        for url in urls:
+            self.handle_url(url, status, search)
+
+    def handle_url(self, url, status, search):
+        try:
+            response = requests.get(url)
+        except:
+            return False
+
+        html = response.content.decode('utf-8')
+        soup = BeautifulSoup(html)
+        [s.extract() for s in soup(['style',
+                                    'script',
+                                    '[document]',
+                                    'head',
+                                    'title'])]
+        visible_text = soup.getText()
+        self.handle_text(visible_text, status, search)
+
+    def handle_text(self, text, status, search):
+        splitted = text.split(" ")
+        max_index = len(splitted)
+
+        responded = False
+        for i in range(max_index - 1):
+            try_officer_name = " ".join(splitted[i:i+2])
+            responses = Command.create_responses(try_officer_name)
+
+            for response, obj, context in responses:
+                context['user'] = status['user']['screen_name']
+                context['obj'] = obj
+                msg = response.get_message(context)
+
+                tweet = TwitterResponse.objects.create(search=search, response=msg, user=context['user'])
+                tweet.send()
+                responded = True
+
+        return responded
+
     def handle(self, *args, **options):
         for search in TwitterSearch.objects.all():
             data = search.search()
 
             statuses = data.get('statuses', [])
-
             for status in statuses:
+                responded = self.handle_text(status['text'], status, search)
 
-                splitted = status['text'].split(" ")
+                urls = re.findall(self.url_regex, status['text'])
+                if urls:
+                    self.handle_urls(urls, status, search)
 
-                max_index = len(splitted)
-
-                not_found = True
-                for i in range(max_index - 1):
-                    try_officer_name = " ".join(splitted[i:i+2])
-                    responses = Command.create_responses(try_officer_name)
-                    not_found = False if responses else True
-
-                    for response, obj, context in responses:
-                        context['user'] = status['user']['screen_name']
-                        context['obj'] = obj
-                        msg = response.get_message(context)
-
-                        tweet = TwitterResponse.objects.create(search=search, response=msg, user=context['user'])
-                        tweet.send()
-
-                if not_found:
+                if responded:
                     print('No result for %s from %s') % (status['text'], status['user']['screen_name'])
 
             if 'search_metadata' in data:
