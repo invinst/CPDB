8db5e3128b580a5e7860a52094e685c6718b3685
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-03-14 13:42:16 +0700

    [c] 665 - fix task `build_test_sass`
    [#115306665] https://www.pivotaltracker.com/story/show/115306665

--- gulp_tasks/build_test_sass.js
+++ gulp_tasks/build_test_sass.js
@@ -3,6 +3,6 @@ var buildSass = require('./utils/build_sass');
 
 module.exports = buildSass({
   src: './common/static/sass/test.sass',
-  dest: './local_static/css',
+  dest: './static/css',
   fileName: 'test.css'
 });
