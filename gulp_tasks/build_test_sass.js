667c307e9974857029f2d9ef7b6b2e8f6578496f
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-03-14 11:15:54 +0700

    [c] 665 - fix build path test sass
    [#115306665] https://www.pivotaltracker.com/story/show/115306665

--- gulp_tasks/build_test_sass.js
+++ gulp_tasks/build_test_sass.js
@@ -3,6 +3,6 @@ var buildSass = require('./utils/build_sass');
 
 module.exports = buildSass({
   src: './common/static/sass/test.sass',
-  dest: './static/css',
+  dest: './local_static/css',
   fileName: 'test.css'
 });
