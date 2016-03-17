c8261c8d61e4898074e6e0e3bf882ed8ccf66606
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-03-17 11:34:56 +0700

    [f] 217 - Added officer_age field to OfficerAllegation model
    [#114655217] https://www.pivotaltracker.com/story/show/114655217

---
+++ common/migrations/0003_officerallegation_officer_age_data.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def calculate_officer_age(apps, schema_editor):
+    OfficerAllegation = apps.get_model('common', 'OfficerAllegation')
+    for oa in OfficerAllegation.objects.filter(
+            allegation__incident_date_only__isnull=False, officer__birth_year__gt=0):
+        try:
+            oa.officer_age = oa.allegation.incident_date_only.year - oa.officer.birth_year
+        except (AttributeError, TypeError):
+            continue
+        print('    %d' % oa.officer_age)
+        oa.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0002_officerallegation_officer_age'),
+    ]
+
+    operations = [
+        migrations.RunPython(calculate_officer_age)
+    ]
--- common/models/__init__.py
+++ common/models/__init__.py
@@ -171,6 +171,7 @@ class OfficerAllegation(models.Model):
         max_length=20, null=True, blank=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
+    officer_age = models.IntegerField(null=True, db_index=True)
     objects = OfficerAllegationManager()
     disciplined = DisciplinedManager()
 
