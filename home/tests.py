2c72d6ed91be843fbee75becdb0442ac792355b4
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-03-29 11:29:25 +0700

    [f] 092 - add glossary block to wagtail admin
    [#107657092] https://www.pivotaltracker.com/story/show/107657092

---
+++ home/tests.py
@@ -0,0 +1,41 @@
+import json
+
+from common.tests.core import SimpleTestCase
+from home.factories import HomePageFactory
+from home.models import HomePage
+
+
+class HomePageModelTestCase(SimpleTestCase):
+    def test_extended_body_prop(self):
+        body = [
+            {
+                'value': [
+                    {
+                        'value': '<p>content</p>',
+                        'type': 'half_paragraph'
+                    }
+                ],
+                'type': 'row_section'
+            },
+            {
+                'type': 'table_section',
+                'value': [
+                    {
+                        'type': 'glossary_table_row',
+                        'value': {
+                            'definition': 'text 2',
+                            'category': 'complaint-process',
+                            'term': 'term'
+                        }
+                    }
+                ]
+            }
+        ]
+        HomePage.get_tree().all().delete()
+        root = HomePage.add_root(instance=HomePageFactory.build(
+            title='Root', slug='root'))
+        homepage = root.add_child(
+            instance=HomePageFactory.build(
+                title='child', body=json.dumps(body), slug='child'))
+
+        homepage.extended_body().should.equal(body)
