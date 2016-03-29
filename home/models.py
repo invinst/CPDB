76505d509bee76daf150cbbf06eaf81dacb3cb51
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-03-29 11:29:25 +0700

    [f] 092 - add glossary block to wagtail admin
    [#107657092] https://www.pivotaltracker.com/story/show/107657092

--- home/models.py
+++ home/models.py
@@ -13,8 +13,24 @@ class HomePage(Page):
         ('row_section', blocks.StreamBlock([
             ('half_paragraph', blocks.RichTextBlock()),
             ('full_paragraph', blocks.RichTextBlock()),
-        ])
-        )], blank=True)
+        ])),
+        ('table_section', blocks.StreamBlock([
+            ('glossary_table_row', blocks.StructBlock([
+                ('term', blocks.CharBlock()),
+                ('definition', blocks.TextBlock()),
+                ('category', blocks.ChoiceBlock(choices=[
+                    ('n_a', 'n/a'),
+                    ('outcomes', 'Outcomes'),
+                    ('organizational', 'Organizational'),
+                    ('complaint-process', 'Complaint Process')
+                    ]))
+                ])),
+            ('table_row', blocks.StreamBlock([
+                ('table_text_cell', blocks.TextBlock()),
+                ('table_char_cell', blocks.CharBlock())
+                ])),
+        ]))
+        ], blank=True)
 
     api_fields = ('slug', 'extended_body')
     content_panels = Page.content_panels + [
@@ -23,8 +39,10 @@ class HomePage(Page):
 
     def extended_body(self):
         body = json.loads(self.serializable_data()['body'])
+
         for row in body:
-            for paragraph in row['value']:
-                paragraph['value'] = expand_db_html(paragraph['value'])
+            if row['type'] == 'row_section':
+                for paragraph in row['value']:
+                    paragraph['value'] = expand_db_html(paragraph['value'])
 
         return body
