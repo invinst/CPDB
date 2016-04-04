38c0f2fe911e80495bf8d5135c58ea22a984807a
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-04-04 09:45:27 +0700

    [r] fix phantomjs, exit when install failed

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -20,5 +20,8 @@ if ! [ $(phantomjs --version) = 2.1.1 ]; then
     sudo chmod a+x $BIN_PATH$PHANTOM_JS
 
     sudo ln -sf $BIN_PATH$PHANTOM_JS/bin/phantomjs /usr/local/bin
+    if ! [ $(phantomjs --version) = 2.1.1 ]; then
+        exit 1
+    fi
 fi
 echo "PhantomJS version $(phantomjs --version)"
