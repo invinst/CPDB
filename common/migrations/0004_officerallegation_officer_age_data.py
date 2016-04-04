e52119aa0c1a30e9ecc56c2f6c84f1bf49c0cfa3
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-04-04 16:20:41 +0700

    [f] 197 - Added fancy output to migration
    [#116550197] https://www.pivotaltracker.com/story/show/116550197

--- common/migrations/0004_officerallegation_officer_age_data.py
+++ common/migrations/0004_officerallegation_officer_age_data.py
@@ -3,17 +3,53 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+from tqdm import tqdm
+
 
 def calculate_officer_age(apps, schema_editor):
     OfficerAllegation = apps.get_model('common', 'OfficerAllegation')
-    for oa in OfficerAllegation.objects.filter(
-            allegation__incident_date_only__isnull=False, officer__birth_year__gt=0):
+
+    oas = OfficerAllegation.objects.filter(
+        allegation__incident_date_only__isnull=False, officer__birth_year__gt=0)
+
+    total_rows = oas.count()
+    pbar = tqdm(total=total_rows)
+    num_calculated = 0
+
+    for oa in oas:
         try:
             oa.officer_age = oa.allegation.incident_date_only.year - oa.officer.birth_year
+            num_calculated += 1
         except (AttributeError, TypeError):
             continue
-        print('    %d' % oa.officer_age)
         oa.save()
+
+        pbar.update()
+        pbar.set_description('  Processed %d officer allegations' % num_calculated)
+
+    pbar.close()
+    print('  Processed %d officer allegations' % num_calculated)
+
+
+def nullify_officer_age(apps, schema_editor):
+    OfficerAllegation = apps.get_model('common', 'OfficerAllegation')
+
+    oas = OfficerAllegation.objects.filter(officer_age__isnull=False)
+
+    total_rows = oas.count()
+    pbar = tqdm(total=total_rows)
+    num_iterated = 0
+
+    for oa in oas:
+        oa.officer_age = None
+        oa.save()
+
+        num_iterated += 1
+        pbar.update()
+        pbar.set_description('  Processed %d rows' % num_iterated)
+
+    pbar.close()
+    print('  Processed %d rows' % num_iterated)
 
 
 class Migration(migrations.Migration):
@@ -23,5 +59,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(calculate_officer_age)
+        migrations.RunPython(calculate_officer_age, nullify_officer_age)
     ]
