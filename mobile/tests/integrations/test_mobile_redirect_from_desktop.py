4d3f906981cb6f98ec595130e49eb8e43dc7036a
Invisible Scream Cat+Invisible Echo <dev@invisibleinstitute.com>
2016-04-06 10:14:43 +0700

    [r] Remove unused tests

--- mobile/tests/integrations/test_mobile_redirect_from_desktop.py
+++ mobile/tests/integrations/test_mobile_redirect_from_desktop.py
@@ -1,20 +1,7 @@
-# from allegation.factories import OfficerFactory
 from allegation.tests.utils.autocomplete_test_helper_mixin import AutocompleteTestHelperMixin
 from common.tests.core import BaseLiveTestCase
-# from common.utils.haystack import rebuild_index
 
 
 class MobileRedirectFromDesktopTest(AutocompleteTestHelperMixin, BaseLiveTestCase):
-    # NOTE: Disable until we fixed it in next commit
-    # def test_redirect(self):
-    #     officer = OfficerFactory()
-    #
-    #     rebuild_index()
-    #     # FIXME: this seems to be a wrong test, please fix!
-    #     self.visit('/')
-    #     self.search_officer(officer)
-    #     self.visit(self.mobile_url_for_current_request())
-    #     self.until(lambda: officer.get_mobile_url() in self.browser.current_url)
-
     def mobile_url_for_current_request(self):
         return self.browser.current_url.replace('data', 'mobile/data')
