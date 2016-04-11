5f2e05a14ed648730e07f4d5074d64447db4a7a7
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-04-11 11:35:22 +0700

    created and use `repr_equal_either` function

--- allegation/tests/models/test_models_utils.py
+++ allegation/tests/models/test_models_utils.py
@@ -1,21 +1,22 @@
 from django.db.models import Case, Value, When, CharField
 
 from allegation.models.utils import get_num_range_case
-from common.tests.core import SimpleTestCase
+from common.tests.core import SimpleTestCase, repr_equal_either
 
 
 class ModelUtilsTestCase(SimpleTestCase):
     def test_get_num_range_case(self):
-        actual_result = get_num_range_case('a', [1, 5])
+        result = get_num_range_case('a', [1, 5])
 
-        try:
-            repr(actual_result).should.equal(
+        repr_equal_either(
+            result,
+            (
                 "<Case: CASE WHEN <Q: (AND: ('a__gte', 1), ('a__lte', 5))> THEN Value(1-5), "
-                "WHEN <Q: (AND: ('a__gte', 6))> THEN Value(6+), ELSE Value(Unknown)>")
-        except AssertionError:
-            repr(actual_result).should.equal(
+                "WHEN <Q: (AND: ('a__gte', 6))> THEN Value(6+), ELSE Value(Unknown)>"),
+            (
                 "<Case: CASE WHEN <Q: (AND: ('a__lte', 5), ('a__gte', 1))> THEN Value(1-5), "
                 "WHEN <Q: (AND: ('a__gte', 6))> THEN Value(6+), ELSE Value(Unknown)>")
+        )
 
     def test_get_num_range_case_single_value(self):
         num_range = [1]
@@ -39,15 +40,16 @@ class ModelUtilsTestCase(SimpleTestCase):
         repr(expect_result).should.equal(repr(actual_result))
 
     def test_get_num_range_case_begin_with_zero(self):
-        actual_result = get_num_range_case('a', [0, 3, 5])
+        result = get_num_range_case('a', [0, 3, 5])
 
-        try:
-            repr(actual_result).should.equal(
+        repr_equal_either(
+            result,
+            (
                 "<Case: CASE WHEN <Q: (AND: ('a__lte', 3))> THEN Value(<3), "
                 "WHEN <Q: (AND: ('a__gte', 4), ('a__lte', 5))> "
-                "THEN Value(4-5), WHEN <Q: (AND: ('a__gte', 6))> THEN Value(6+), ELSE Value(Unknown)>")
-        except AssertionError:
-            repr(actual_result).should.equal(
+                "THEN Value(4-5), WHEN <Q: (AND: ('a__gte', 6))> THEN Value(6+), ELSE Value(Unknown)>"),
+            (
                 "<Case: CASE WHEN <Q: (AND: ('a__lte', 3))> THEN Value(<3), "
                 "WHEN <Q: (AND: ('a__lte', 5), ('a__gte', 4))> "
                 "THEN Value(4-5), WHEN <Q: (AND: ('a__gte', 6))> THEN Value(6+), ELSE Value(Unknown)>")
+        )
