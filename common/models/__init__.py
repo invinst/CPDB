56820468b7478514ce0625a9934c7436dd1a91a1
Invisible Pineapple + Invisible Passion <dev@invisibleinstitute.com>
2016-04-15 11:01:23 +0700

    Revert "Merge pull request #750 from EastAgile/feature/941-merge-new-officer-data"
    This reverts commit 6fa0365ba7d6061b2ac91d11f275f3351c240f08, reversing
    changes made to a4733fddb191e0e39f018354bf8046fd0c917244.

--- common/models/__init__.py
+++ common/models/__init__.py
@@ -234,11 +234,3 @@ class PendingPdfAllegation(models.Model):
 class DocumentCrawler(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     num_documents = models.IntegerField()
-
-
-class OfficerAlias(models.Model):
-    old_officer_id = models.IntegerField()
-    new_officer = models.ForeignKey(Officer)
-
-    class Meta:
-        unique_together = (('old_officer_id', 'new_officer'))
