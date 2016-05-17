5ce242d44a70ce323a382ef2eae95d71f65731e5
Invisible Uniform+Invisible Wise Monkeys <dev@invisibleinstitute.com>
2016-05-17 10:38:25 +0700

    [f] 555 - Change to new auth params passing
    [#119423555] https://www.pivotaltracker.com/story/show/119423555

--- twitterbot/management/commands/start_twitterbot.py
+++ twitterbot/management/commands/start_twitterbot.py
@@ -5,10 +5,12 @@ from responsebot.responsebot import ResponseBot
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        options['consumer_key'] = settings.TWITTER_CONSUMER_KEY
-        options['consumer_secret'] = settings.TWITTER_CONSUMER_SECRET
-        options['token_key'] = settings.TWITTER_APP_TOKEN_KEY
-        options['token_secret'] = settings.TWITTER_APP_TOKEN_SECRET
+        options['auth'] = (
+            settings.TWITTER_CONSUMER_KEY,
+            settings.TWITTER_CONSUMER_SECRET,
+            settings.TWITTER_APP_TOKEN_KEY,
+            settings.TWITTER_APP_TOKEN_SECRET
+        )
         options['handlers_package'] = 'twitterbot.handlers'
         options['user_stream'] = True
 
